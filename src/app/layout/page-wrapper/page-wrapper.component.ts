eb300a4fdede78e6862352cfd41da212695a2897
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-20 17:15:31 +0700

    create pagewrapper

---
+++ src/app/layout/page-wrapper/page-wrapper.component.ts
@@ -0,0 +1,14 @@
+import { Input, OnInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-wrapper',
+  templateUrl: './page-wrapper.component.html',
+  styleUrls: ['./page-wrapper.component.scss'],
+})
+export class PageWrapperComponent implements OnInit {
+  @Input() title?: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
