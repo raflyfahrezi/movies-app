101df921133c516bc4a365f07438cf0e94bc5592
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-20 20:45:01 +0700

    create app env

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  // environment
+  production: true,
+
+  //   api
+  baseUrl: 'https://api.themoviedb.org/3/',
+  apiKey: 'e020b4926367bed54b0932937c78afd1',
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  // environment
+  production: false,
+
+  //   api
+  baseUrl: 'https://api.themoviedb.org/3/',
+  apiKey: 'e020b4926367bed54b0932937c78afd1',
+};
