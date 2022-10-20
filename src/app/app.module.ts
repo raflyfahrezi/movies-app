8d7880b9646a98623c144ec232e99aebde194c70
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-20 20:45:52 +0700

    fix app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { NgHelmetModule } from 'ng-helmet';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './modules/home/home.component';
-import { PageWrapperComponent } from './layout/page-wrapper/page-wrapper.component';
 import { HeadComponent } from './layout/head/head.component';
+import { HomeComponent } from './modules/home/home.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { PageWrapperComponent } from './layout/page-wrapper/page-wrapper.component';
 
 @NgModule({
   providers: [],
   bootstrap: [AppComponent],
-  declarations: [AppComponent, HomeComponent, PageWrapperComponent, HeadComponent, FooterComponent, WrapperComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeadComponent,
+    FooterComponent,
+    WrapperComponent,
+    PageWrapperComponent,
+  ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     NgHelmetModule.forRoot({
       baseTitle: '| Movies App',
     }),
