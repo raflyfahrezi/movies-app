ff7a54daa26594e76f005e921b26b7959d47c253
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-20 20:45:25 +0700

    fix home modules

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,12 +1,21 @@
 import { OnInit, Component } from '@angular/core';
 
+import { Movie } from '@/models';
+import { MovieService } from '@/services';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  movieList: Movie[] = [];
 
-  ngOnInit(): void {}
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.movieService.getDiscoverMovies().subscribe((response) => {
+      this.movieList = response.results;
+    });
+  }
 }
