ad6bd335f4abe2a3afc8b568b7be64027f189841
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-10-21 17:50:23 +0700

    fix a[[

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NgHelmetModule } from 'ng-helmet';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { routes } from '@/routes';
 
 import { AppComponent } from './app.component';
 import { HeadComponent } from './layout/head/head.component';
@@ -11,6 +14,8 @@ import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { PageWrapperComponent } from './layout/page-wrapper/page-wrapper.component';
 import { CardComponent } from './components/card/card.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './modules/about/about.component';
+import { MovieComponent } from './modules/movie/movie.component';
 
 @NgModule({
   providers: [],
@@ -24,10 +29,13 @@ import { HeaderComponent } from './components/header/header.component';
     PageWrapperComponent,
     CardComponent,
     HeaderComponent,
+    AboutComponent,
+    MovieComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
     NgHelmetModule.forRoot({
       baseTitle: '| Movies App',
     }),
